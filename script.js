7f16c676322e77c3ae075b47411d70028ff75275
Alex <ale8under8@gmail.com>
2019-03-02 17:01:23 -0800

    add tweet and tumblr func  script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 $(document).ready(() => {
+	let randomQuote = '';
+	let randomAuthor = '';
 	const getQuote = () => {
-		const quotes = ['Hello', 'Hi', 'Yeah'];
-		const authors = ['A', 'B', 'C'];
-		const randomNum = Math.floor(Math.random()*quotes.length);
-		const randomQuote = quotes[randomNum];
-		const randomAuthor = authors[randomNum];
+		let quotes = ['Hello', 'Hi', 'Yeah'];
+		let authors = ['A', 'B', 'C'];
+		let randomNum = Math.floor(Math.random()*quotes.length);
+		randomQuote = quotes[randomNum];
+		randomAuthor = authors[randomNum];
 		$('#text').text(randomQuote);
 		$('#author').text(randomAuthor);
 	};
+	getQuote();
+	$('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + randomQuote + " - " + randomAuthor);
+	$('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(randomAuthor)+'&content=' + encodeURIComponent(randomQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
 	$('#new-quote').on('click', () => getQuote());
 });
+
+
