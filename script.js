e7b3200d4eb5a8246449282f56e1005e4a88e406
Alex <ale8under8@gmail.com>
2019-03-03 18:43:11 -0800

    add animation script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+  const fadeInTime = 1000;
+  const fadeOutTime = 100;
 	let currentQuote;
 	let currentAuthor;
 	const getQuote = () => {
@@ -9,11 +11,17 @@ $(document).ready(() => {
       currentQuote = data.quoteText;
       currentAuthor = data.quoteAuthor;
       $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + currentQuote + " - " + currentAuthor);
-	    $('#tumblr-quote').attr('href', 'http://tumblr.com/widgets/share/tool?canonicalUrl=http://cats.com');
+      $('#tumblr-quote').attr('href', 'http://tumblr.com/widgets/share/tool?canonicalUrl=http://cats.com');
     });
 	};
-	getQuote();
-	$('#new-quote').on('click', () => getQuote());
+  getQuote();
+  $('#new-quote').on('click', () => {
+    $('#text').fadeOut(fadeOutTime);
+    $('#author').fadeOut(fadeOutTime);
+    $('#text').fadeIn(fadeInTime);
+    $('#author').fadeIn(fadeInTime);
+    getQuote()
+  });
 });
 
 
